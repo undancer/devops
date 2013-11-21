15d72bec819ee31373d2ed84d54d198b81421577
undancer <undancer@gmail.com>
2013-11-22 00:16:12 +0800

    修改路径

--- modules/nginx/manifests/fetch.pp
+++ modules/nginx/manifests/fetch.pp
@@ -1,3 +1,3 @@
 class nginx::fetch {
-  file { "/tmp/nginx.tar.gz": source => "puppet:///modules/nginx/files/nginx-1.4.4.tar.gz" }
+  file { "/tmp/nginx.tar.gz": source => "puppet://$puppetserver/root/devops/modules/nginx/files/nginx-1.4.4.tar.gz" }
 }
