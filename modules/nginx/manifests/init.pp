834e3bf46901715ca562c44605df708fbbac88cd
undancer <undancer@gmail.com>
2013-11-22 00:27:06 +0800

    创建nginx的用户和组

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,3 +1,11 @@
 class nginx {
+  user { "nginx":
+    ensure => present,
+    comment => "nginx user"
+  }
+
+  group { "nginx":
+    ensure => present
+  }
   include nginx::fetch, nginx::install
 }
