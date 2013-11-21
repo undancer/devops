b86758d110eb5e22eeb236a2a45064e2f35c1141
undancer <undancer@gmail.com>
2013-11-22 01:57:44 +0800

    nginx配置脚本

---
+++ modules/nginx/manifests/service.pp
@@ -0,0 +1,9 @@
+class nginx::service {
+  service {"nginx":
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    hasstatus => true,
+    subscribe => [Class[nginx::fetch],Class[nginx::install],Class[nginx::config]]
+  }
+}
