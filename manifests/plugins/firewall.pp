d95216bc5db70b2ac00d43e170c2988d4e1c783a
undancer <undancer@gmail.com>
2013-11-22 18:21:41 +0800

    安装官方的模块

--- manifests/plugins/firewall.pp
+++ manifests/plugins/firewall.pp
@@ -1,1 +1,5 @@
-exec { "install-firewall": creates => "/etc/puppet/modules/firewall", command => "/usr/bin/puppet module install puppetlabs-firewall" }
+exec { "install-firewall":
+  creates => "/etc/puppet/modules/firewall",
+  command => "/usr/bin/puppet module install puppetlabs-firewall",
+  stage => pre
+}
